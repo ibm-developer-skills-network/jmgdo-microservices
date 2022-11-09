84a89e64dd5b9d2871ad096864a38ce13667a823
lavskillup <73978832+lavskillup@users.noreply.github.com>
2022-11-09 11:27:43 +0530

    Update products.py

--- CRUD/products.py
+++ CRUD/products.py
@@ -11,39 +11,8 @@ products = [
     {'id': 144, 'name': 'Black Marker', 'price': 1.99}
 ]
 
-@app.route('/products', methods=['GET'])
-def get_products():
-    return jsonify(products)
-
-
-@app.route('/products/<id>', methods=['GET'])
-def get_product(id):
-    id = int(id)
-    product = [x for x in products if x["id"] == id][0]
-    return jsonify(product)
-
-
-@app.route('/products', methods=['POST'])
-def add_product():
-    products.append(request.get_json())
-    return '', 201
-
-
-@app.route('/products/<id>', methods=['PUT'])
-def update_product(id):
-    id = int(id)
-    updated_product = json.loads(request.data)
-    product = [x for x in products if x["id"] == id][0]
-    for key, value in updated_product.items():
-        product[key] = value
-    return '', 204
-
-
-@app.route('/products/<id>', methods=['DELETE'])
-def remove_product(id):
-    id = int(id)
-    product = [x for x in products if x["id"] == id][0]
-    products.remove(product)
-    return '', 204
+#
+#Add all the REST API end-points here
+#
 
 app.run(port=5000,debug=True)
